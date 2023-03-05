a3f95b5318154b9c66d5ab16480423d124859e22
aoksy1 <93058382+aoksy1@users.noreply.github.com>
2023-03-05 12:34:50 +0100

    v6
    Changed Socket connection to have timeout, if is a problem with connecting to server it will not be stucked in loop. Changed error handling to output new log if error occurs.

--- app/src/main/java/com/example/seii_einzelnbeispiel/TCPConnection.java
+++ app/src/main/java/com/example/seii_einzelnbeispiel/TCPConnection.java
@@ -9,46 +9,49 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class TCPConnection extends Thread {
     private Socket clientSocket;
-    private String hostname;
-    private int port;
-    private TextView responseText;
-    private String matriculationNumber;
+    private final String hostname;
+    private final int port;
+    private final TextView responseTextView;
+    private final String matriculationNumber;
 
     public TCPConnection(String hostname, int port, TextView responseText, String matriculationNumber) {
         this.hostname = hostname;
         this.port = port;
-        this.responseText = responseText;
+        this.responseTextView = responseText;
         this.matriculationNumber = matriculationNumber;
     }
 
     public void run(){
         try {
-            clientSocket = new Socket(hostname, port);
+            clientSocket = new Socket();
+            clientSocket.connect(new InetSocketAddress(hostname, port), 5000);
 
             DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
             BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             outToServer.writeBytes(matriculationNumber + '\n');
+            
             final String response = inFromServer.readLine();
-            responseText.post(new Runnable() {
+            responseTextView.post(new Runnable() {
                 @Override
                 public void run() {
-                    responseText.setText(response);
+                    responseTextView.setText(response);
                 }
             });
 
         } catch (IOException e) {
-            e.printStackTrace();
+            Log.e("TCPConnection", "Error", e);
         }
         finally {
             if(clientSocket != null){
                 try {
                     clientSocket.close();
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    Log.e("TCPConnection", "Error closing socket", e);
                 }
             }
         }
