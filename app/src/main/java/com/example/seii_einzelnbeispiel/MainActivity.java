7e7dc74eac5b82a903517985af974f5f124b4de2
aoksy1 <93058382+aoksy1@users.noreply.github.com>
2023-03-05 13:55:56 +0100

    v7
    Completed method for quer summe calculation and tested the output. Added onClickListener for Calculate button. To Do is to convert the given result to binary and create tests for the methods.

--- app/src/main/java/com/example/seii_einzelnbeispiel/MainActivity.java
+++ app/src/main/java/com/example/seii_einzelnbeispiel/MainActivity.java
@@ -12,25 +12,36 @@ public class MainActivity extends AppCompatActivity {
     final String hostname = "se2-isys.aau.at";
     final int port = 53212;
     Button btnSendRequest;
+    Button btnCalculate;
     EditText txtMatriculationNumber;
     TextView txtResponseText;
+    String matriculationNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        String matriculationNumber;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnSendRequest = (Button) findViewById(R.id.btnSend);
+        btnCalculate = (Button) findViewById(R.id.btnCalculate);
         txtMatriculationNumber = (EditText) findViewById(R.id.editTextNumberSigned);
-        matriculationNumber = txtMatriculationNumber.getText().toString();
         txtResponseText = (TextView) findViewById(R.id.textView2);
 
-
         btnSendRequest.setOnClickListener(v -> {
-            if(!matriculationNumber.isEmpty() && matriculationNumber.matches("[0-9]")){
+            matriculationNumber = txtMatriculationNumber.getText().toString();
+            if(!matriculationNumber.isEmpty() && matriculationNumber.matches("[0-9]+")){
                 TCPConnection tcpConnection = new TCPConnection(hostname, port, txtResponseText, matriculationNumber);
                 tcpConnection.start();
+            }
+            else if(matriculationNumber.isEmpty()) Toast.makeText(getApplicationContext(), "Please enter matricualation number!", Toast.LENGTH_SHORT).show();
+            else Toast.makeText(getApplicationContext(), "Matriculation number must contain only numbers!", Toast.LENGTH_SHORT).show();
+        });
+
+        btnCalculate.setOnClickListener(v -> {
+            matriculationNumber = txtMatriculationNumber.getText().toString();
+            if(!matriculationNumber.isEmpty() && matriculationNumber.matches("[0-9]+")){
+                Rechner rechner = new Rechner(txtResponseText);
+                rechner.querSumme(matriculationNumber);
             }
             else if(matriculationNumber.isEmpty()) Toast.makeText(getApplicationContext(), "Please enter matricualation number!", Toast.LENGTH_SHORT).show();
             else Toast.makeText(getApplicationContext(), "Matriculation number must contain only numbers!", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/seii_einzelnbeispiel/Rechner.java
+++ app/src/main/java/com/example/seii_einzelnbeispiel/Rechner.java
@@ -1,8 +1,31 @@
 package com.example.seii_einzelnbeispiel;
 
+import android.util.Log;
+import android.widget.TextView;
+
 public class Rechner {
+    private final TextView responseTextView;
+
+    public Rechner(TextView responseTextView) {
+        this.responseTextView = responseTextView;
+    }
+
     public int querSumme(String matriculationNumber){
-        return 0;
+        int numbersSum = 0;
+        for(char number : matriculationNumber.toCharArray()){
+            numbersSum += Character.getNumericValue(number);
+        }
+
+        Log.e("TAG", "amar "+numbersSum);
+
+        final int result = numbersSum;
+        responseTextView.post(new Runnable() {
+            @Override
+            public void run() {
+                responseTextView.setText(String.valueOf(result));
+            }
+        });
+        return result;
     }
 
     public String intToBinary(int number){
