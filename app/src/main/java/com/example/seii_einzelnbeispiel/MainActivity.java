ab7a7732bea588a7d8eeb51b9054bbbc58214a02
aoksy1 <93058382+aoksy1@users.noreply.github.com>
2023-03-05 13:34:23 +0100

    v6
    Added some error handling if send button is pressed and nothing is being entered to tell to user that Matrikelnummber cannot be empty or contain letters. Then I have added a new button to our UI for next part of the assignment sheet. I have created new class for calculations and created two empty methods for quer summe and int to binary conversation.

--- app/src/main/java/com/example/seii_einzelnbeispiel/MainActivity.java
+++ app/src/main/java/com/example/seii_einzelnbeispiel/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     final String hostname = "se2-isys.aau.at";
@@ -16,17 +17,23 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String matriculationNumber;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnSendRequest = (Button) findViewById(R.id.btnSend);
         txtMatriculationNumber = (EditText) findViewById(R.id.editTextNumberSigned);
+        matriculationNumber = txtMatriculationNumber.getText().toString();
         txtResponseText = (TextView) findViewById(R.id.textView2);
 
 
         btnSendRequest.setOnClickListener(v -> {
-            TCPConnection tcpConnection = new TCPConnection(hostname, port, txtResponseText, txtMatriculationNumber.getText().toString());
-            tcpConnection.start();
+            if(!matriculationNumber.isEmpty() && matriculationNumber.matches("[0-9]")){
+                TCPConnection tcpConnection = new TCPConnection(hostname, port, txtResponseText, matriculationNumber);
+                tcpConnection.start();
+            }
+            else if(matriculationNumber.isEmpty()) Toast.makeText(getApplicationContext(), "Please enter matricualation number!", Toast.LENGTH_SHORT).show();
+            else Toast.makeText(getApplicationContext(), "Matriculation number must contain only numbers!", Toast.LENGTH_SHORT).show();
         });
     }
 }
---
+++ app/src/main/java/com/example/seii_einzelnbeispiel/Rechner.java
@@ -0,0 +1,11 @@
+package com.example.seii_einzelnbeispiel;
+
+public class Rechner {
+    public int querSumme(String matriculationNumber){
+        return 0;
+    }
+
+    public String intToBinary(int number){
+        return "";
+    }
+}
