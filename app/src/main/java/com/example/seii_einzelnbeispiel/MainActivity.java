86e086d20fccfa2fb40920edbc057bb74574011c
aoksy1 <93058382+aoksy1@users.noreply.github.com>
2023-03-05 02:48:22 +0100

    v3
    Created Socket that is connecting to us given server, but not yet sending or receving data from it!

--- app/src/main/java/com/example/seii_einzelnbeispiel/MainActivity.java
+++ app/src/main/java/com/example/seii_einzelnbeispiel/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.seii_einzelnbeispiel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    static String hostname = "se2-isys.aau.at";
+    static int port = 53212;
+    Button btnSendRequest;
+    EditText txtMatriculationNumber;
+    TextView txtResponseText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSendRequest = (Button) findViewById(R.id.btnSend);
+        txtMatriculationNumber = (EditText) findViewById(R.id.editTextNumberSigned);
+        txtResponseText = (TextView) findViewById(R.id.textView2);
+
+        TCPConnection tcpConnection = new TCPConnection(hostname, port, txtResponseText);
+        tcpConnection.start();
     }
 }
--- app/src/main/java/com/example/seii_einzelnbeispiel/TCPConnection.java
+++ app/src/main/java/com/example/seii_einzelnbeispiel/TCPConnection.java
@@ -1,5 +1,35 @@
 package com.example.seii_einzelnbeispiel;
 
-public class TCPConnection {
-    
+import android.widget.TextView;
+import java.io.IOException;
+import java.net.Socket;
+
+public class TCPConnection extends Thread {
+    private Socket socket;
+    private String hostname;
+    private int port;
+    private TextView responseText;
+
+    public TCPConnection(String hostname, int port, TextView responseText) {
+        this.hostname = hostname;
+        this.port = port;
+        this.responseText = responseText;
+    }
+
+    public void run(){
+        try {
+            socket = new Socket(hostname, port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if(socket != null){
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
